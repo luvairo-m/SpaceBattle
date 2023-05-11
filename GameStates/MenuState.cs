d2d590ad84ce095aabdb60e1bad618c9a126ae3e
denissotnikov4 <sotnikovdenis04@gmail.com>
2023-05-11 19:53:32 +0500

    Added background to Menu State

--- GameStates/MenuState.cs
+++ GameStates/MenuState.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using SpaceBattle.Controls;
+using SpaceBattle.Models;
 
 namespace SpaceBattle.GameStates;
 
@@ -11,11 +12,17 @@ public class MenuState : State
 {
     private readonly List<Component> components;
 
+    private readonly Texture2D backgroundSprite;
+
+    private readonly Background background1;
+    private readonly Background background2;
+
     public MenuState(Game1 game, GraphicsDeviceManager graphicsDevice, ContentManager content) 
         : base(game, content, graphicsDevice)
     {
         var buttonTexture = content.Load<Texture2D>("Controls/ButtonYellow");
         var buttonFont = content.Load<SpriteFont>("Fonts/Font");
+        backgroundSprite = content.Load<Texture2D>("Backgrounds/simple-space-background");
 
         var loadGameButton = new Button(buttonTexture, buttonFont)
         {
@@ -39,11 +46,21 @@ public class MenuState : State
             loadGameButton,
             quitGameButton,
         };
+
+        background1 = new Background(new(0, -Game1.WindowHeight), 250);
+        background2 = new Background(new(), 250);
     }
 
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         spriteBatch.Begin();
+
+        spriteBatch.Draw(backgroundSprite,
+                new Rectangle((int)background1.Position.X, (int)background1.Position.Y,
+                Game1.WindowWidth, Game1.WindowHeight), Color.White);
+        spriteBatch.Draw(backgroundSprite,
+                new Rectangle((int)background2.Position.X, (int)background2.Position.Y,
+                Game1.WindowWidth, Game1.WindowHeight), Color.White);
 
         foreach (var component in components)
             component.Draw(gameTime, spriteBatch);
@@ -55,6 +72,9 @@ public class MenuState : State
     
     public override void Update(GameTime gameTime)
     {
+        background1.Update(gameTime);
+        background2.Update(gameTime);
+
         foreach (var component in components)
             component.Update(gameTime);
     }
